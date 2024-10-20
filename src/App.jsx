4f69edef87e2e9cb5e96ed23ff860ff2fc67d81b
Sean Fox <seandfox@proton.me>
2024-10-20 09:04:31 -0400

    removed src from filepaths

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import {
   Route,
 } from 'react-router-dom';
 
-import Home from '/src/pages/Home/Home';
-import Liam from '/src/components/1.liam/Liam';
-import Zac from '/src/components/2.zac/Zac';
-import Morgan from '/src/components/3.morgan/Morgan';
-import Jenna from '/src/components/4.jenna/Jenna';
-import Navbar from '/src/components/a.navbar/Navbar';
-import ExText from '/src/components/b.exhibition-text/ExhibitionText';
-import About from '/src/components/c.about/About';
+import Home from './pages/Home/Home';
+import Liam from './components/1.liam/Liam';
+import Zac from './components/2.zac/Zac';
+import Morgan from './components/3.morgan/Morgan';
+import Jenna from './components/4.jenna/Jenna';
+import Navbar from './components/a.navbar/Navbar';
+import ExText from './components/b.exhibition-text/ExhibitionText';
+import About from './components/c.about/About';
 
 const App = () => {
   return (
