007ee8f805a529070b631d23cfeb5c3e25f6bb3d
Sean Fox <seandfox@proton.me>
2024-10-20 09:11:38 -0400

    test

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import main0 from './assets/0.main/4x4-main0.webp';
-import main1 from './assets/0.main/4x4-main1.webp';
-import main2 from './assets/0.main/4x4-main2.webp';
-import main3 from '/assets/0.main/4x4-main3.webp';
+import main0 from '/src/assets/0.main/4x4-main0.webp';
+import main1 from '/src/assets/0.main/4x4-main1.webp';
+import main2 from '/src/assets/0.main/4x4-main2.webp';
+import main3 from '/src/assets/0.main/4x4-main3.webp';
 
 const Home = () => {
   return (
